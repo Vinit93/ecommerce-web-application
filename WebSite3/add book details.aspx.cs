3feac870d2975c8966b7b2fb683eba65c7e9e4a3
Unknown <unknown@example.com>
2018-01-29 17:34:22 +0530

    file upload (vinit)

---
+++ WebSite3/add book details.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class add_book_details : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if(FileUpload1.HasFile )
+        {
+            //string fname = FileUpload1.FileName.ToString();
+            string img = "4" + ".jpg";
+            FileUpload1.PostedFile.SaveAs(Server.MapPath("~/image/")+img);
+
+        }
+    }
+}
